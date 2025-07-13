88bf6375ae37b0318a9858dd08dbae5b4207fc39
Kush0302 <kush0302.patel@gmail.com>
2025-07-13 16:28:05 +0530

    "Add status filtering and sorting to job list (Day 6)"

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
-from .import views
+from django.urls import path  # Imports the path() function to define routes
+from .import views # Imports views from the current app
 
 #path('jobs/') This maps the URL /jobs/ to your job_list view
 #views.job_list Calls the view function you just wrote
 #name='job_list' Useful for linking to this view later using Django’s template tags
 urlpatterns=[path('', views.job_list, name='job_list'),
              path('add/', views.add_job, name='add_job'),
-             path('job/<int:pk>/', views.job_detail, name='job_detail'),
-             path('job/<int:pk>/edit/', views.edit_job, name='edit_job'),
+             path('job/<int:pk>/', views.job_detail, name='job_detail'), # URL for viewing details of one job 'pk'stands for(Primary Key)
+             path('job/<int:pk>/edit/', views.edit_job, name='edit_job'), # URL for editing a job
             ]
--- tracker/views.py
+++ tracker/views.py
@@ -4,9 +4,29 @@ from .models import JobApplication
 from .forms import AddJobForm
 from django.contrib import messages
 
-def job_list(request):#request is a built-in object that represents the HTTP request sent by the browser
+def job_list(request): #request is a built-in object that represents the HTTP request sent by the browser
+    status_filter=request.GET.get('status')
+    sort_by=request.GET.get('sort', 'application_date')  #default sorting
+
+
     jobs=JobApplication.objects.all()
-    return render(request, 'tracker/job_list.html',{'jobs': jobs})
+
+#If a specific status is selected (not "All"), the queryset is filtered to show only jobs with that status
+    if status_filter and status_filter!='All':
+        jobs=jobs.filter(status=status_filter)
+
+#If the sorting choice is valid, it sorts the jobs accordingly
+    if sort_by in['application_date', 'company_name']:
+        jobs=jobs.order_by(sort_by)
+
+    status_options=['All', 'Applied', 'Interview', 'Offer', 'Rejected']
+        
+#Renders the updated list to the job_list.html template, passing the filtered and sorted jobs list, and the selected values to prefill the form
+    return render(request, 'tracker/job_list.html',{'jobs': jobs,
+         'selected_status': status_filter or 'All',
+         'selected_sort': sort_by,
+         'status_options' :status_options,
+    })
 
 def add_job(request):
     if request.method=='POST':
@@ -23,17 +43,20 @@ def add_job(request):
     return render(request, 'tracker/add_job.html', {'form': form})
 
 def job_detail(request, pk):
+# Tries to get the job with the given ID. If not found, shows a 404 error page.
     job = get_object_or_404(JobApplication, pk=pk)
+ # Renders the job_detail.html template and passes the job data to it
     return render(request, 'tracker/job_detail.html', {'job': job})
 
 def edit_job(request, pk):
     job = get_object_or_404(JobApplication, pk=pk)
     if request.method == 'POST':
+# Populate form with submitted data and link it to the existing job
         form = AddJobForm(request.POST, instance=job)
         if form.is_valid():
             form.save()
             return redirect('job_detail', pk=job.pk)
     else:
-        form = AddJobForm(instance=job)
+        form = AddJobForm(instance=job)  # If GET request, pre-fill the form with the existing job's data
     return render(request, 'tracker/edit_job.html', {'form': form})
 
